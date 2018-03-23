a65c49277f7745089ea4ca1dac07895bb3abad53
park kyung suk <boardguy1024@gmail.com>
2018-03-23 14:59:10 +0900

    CLLocationManager+Rx追加

---
+++ starter/Wundercast/Extensions/CLLocationManager+Rx.swift
@@ -0,0 +1,41 @@
+//
+//  CLLocationManager+Rx.swift
+//  Wundercast
+//
+//  Created by park kyung suk on 2018/03/23.
+//  Copyright © 2018年 Razeware LLC. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+import RxSwift
+import RxCocoa
+
+extension CLLocationManager: HasDelegate {
+    public typealias Delegate = CLLocationManagerDelegate
+}
+
+class RxCLLocationManagerDelegateProxy: DelegateProxy<CLLocationManager, CLLocationManagerDelegate>, DelegateProxyType, CLLocationManagerDelegate {
+    
+    public weak private(set) var locationManager: CLLocationManager?
+    
+    public init(locationManager: ParentObject) {
+        self.locationManager = locationManager
+        super.init(parentObject: locationManager, delegateProxy: RxCLLocationManagerDelegateProxy.self)
+    }
+    
+    static func registerKnownImplementations() {
+        self.register { RxCLLocationManagerDelegateProxy(locationManager: $0) }
+    }
+}
+
+extension Reactive where Base: CLLocationManager {
+    public var delegate: DelegateProxy<CLLocationManager, CLLocationManagerDelegate> {
+        return RxCLLocationManagerDelegateProxy.proxy(for: base)
+    }
+    
+    var didUpdateLocations: Observable<[CLLocation]> {
+        return delegate.methodInvoked(#selector(CLLocationManagerDelegate.locationManager(_:didUpdateLocations:)))
+            .map { $0.first as! [CLLocation] }
+    }
+}
